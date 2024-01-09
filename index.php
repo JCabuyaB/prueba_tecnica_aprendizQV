9fca10911910dcce852ef66ef2f4e80d1a806b5a
Camilo Cabuya Berrio <cabuyaberriojin@gmail.com>
2024-01-09 16:38:23 -0500

    Maquetación

---
+++ index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IzyAcademy</title>
+    <link rel="stylesheet" href="assets/estilos.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <!-- logo -->
+            <div class="navbar__logo">
+                <img src="" alt="Logo">
+            </div>
+            
+            <!-- menu -->
+            <ul class="menu">
+                <li class="menu__item">Inicio</li>
+                <li class="menu__item">Rutas De Formación
+                    <!-- submenu -->
+                    <ul class="submenu">
+                        <li class="submenu__item">Científico De Datos</li>
+                        <li class="submenu__item">Ruta de Formación En .NET</li>
+                        <li class="submenu__item">Ruta de Formación en Automatización</li>
+                    </ul>
+                </li>
+            </ul>
+
+            <div class="login-container">
+                <!-- person -->
+                <span class="btn-login">
+                    Iniciar Sesión
+                </span>
+                <!-- search -->
+            </div>
+        </nav>
+
+        <div class="banner">
+            <!-- coming soon -->
+        </div>
+    </header>
+
+    <!-- inicio de sesión -->
+    <div class="login-container">
+        <form action="login.php" class="form">
+            <h3 class="form__title">Inicie sesión en su cuenta</h3>
+
+            <!-- campos -->
+            <input type="text" name="user" placeholder="Nombre de Usuario">
+            <input type="password" name="password" placeholder="Contraseña">
+
+            <input type="submit" value="Acceder">
+
+            <!-- botón de cierre de formulario  -->
+            <span class="form__btn-close">&times;</span>
+        </form>
+    </div>
+    
+    <!-- contenido -->
+    <!-- sección de noticias -->
+    <section class="news-section">
+        <h2 class="section-title">Novedades</h2>
+
+        <div class="news">
+            <!-- columna izquierda -->
+            <div class="news-left-column">
+                <div class="news-left__card">
+                    <img src="" alt="imagen_noticia" class="news-left__img">
+                    <div class="news-texts">
+                        <h3 class="news-left__title"></h3>
+                        <p class="news-left__text"></p>
+                    </div>
+                </div>
+            </div>
+
+            <!-- columna derecha -->
+            <div class="news-right-column">
+                <div class="news-right__card">
+                    <img src="" alt="imagen_noticia" class="news-right__img">
+                    <div class="news-texts">
+                        <h3 class="news-right__title"></h3>
+                        <p class="news-right__text"></p>
+                    </div>
+                </div>
+                <div class="news-right__card">
+                    <img src="" alt="imagen_noticia" class="news-right__img">
+                    <div class="news-texts">
+                        <h3 class="news-right__title"></h3>
+                        <p class="news-right__text"></p>
+                    </div>
+                </div>
+                <div class="news-right__card">
+                    <img src="" alt="imagen_noticia" class="news-right__img">
+                    <div class="news-texts">
+                        <h3 class="news-right__title"></h3>
+                        <p class="news-right__text"></p>
+                    </div>
+                </div>
+                <div class="news-right__card">
+                    <img src="" alt="imagen_noticia" class="news-right__img">
+                    <div class="news-texts">
+                        <h3 class="news-right__title"></h3>
+                        <p class="news-right__text"></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- sección aliados -->
+    <section class="partners-section">
+        <h2 class="section-title">Aliados</h2>
+
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+        <div class="partner"><img src="" alt="partner_logo" class="partner_logo"></div>
+    </section>
+
+    <!-- formulario de registro -->
+    section.
+</body>
+</html>
