b2d1c7a01fa98ce9c2e2bf951f2ad2f9ef89ed4f
Camilo Cabuya Berrio <cabuyaberriojin@gmail.com>
2024-01-10 09:32:46 -0500

    maquetación

--- index.php
+++ index.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IzyAcademy</title>
+    <!-- estilos -->
     <link rel="stylesheet" href="assets/estilos.css">
+    <!-- iconos -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 <body>
-    <header>
+    <header class="header">
         <nav class="navbar">
             <!-- logo -->
             <div class="navbar__logo">
-                <img src="" alt="Logo">
+                <img src="https://izyacademy.com/pluginfile.php/1/theme_edumy/headerlogo1/1704492337/Logo%20izyacademy%20transparente2022.png" alt="Logo">
             </div>
             
             <!-- menu -->
@@ -43,7 +46,7 @@
 
     <!-- inicio de sesión -->
     <div class="login-container">
-        <form action="login.php" class="form">
+        <form action="login.php" class="login__form">
             <h3 class="form__title">Inicie sesión en su cuenta</h3>
 
             <!-- campos -->
@@ -123,6 +126,135 @@
     </section>
 
     <!-- formulario de registro -->
-    section.
+    <section class="register-section">
+        <div class="register-left-column">
+            <img src="" alt="img_registro">
+        </div>
+
+        <div class="register-right-column">
+            <h3 class="section-title">Regístrate</h3>
+
+            <form action="registro.php" class="register__form">
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" id="nombre">
+                </div>
+
+                <div class="form-group">
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" name="apellidos" id="apellidos">
+                </div>
+
+                <div class="form-group">
+                    <label for="correo">Correo electrónico</label>
+                    <input type="email" name="correo" id="correo">
+                </div>
+
+                <div class="form-group">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="user-pass" id="password">
+                </div>
+
+                <div class="form-group">
+                    <label for="confirm-password">Confirmar contraseña</label>
+                    <input type="password" name="user-pass2" id="confirm-password">
+                </div>
+
+                <div class="form-group">
+                    <input type="checkbox" name="chk-terminos" id="terminos"> 
+                    <label for="terminos">Acepto términos y condiciones</label>
+
+                    <input type="checkbox" name="chk-politica" id="politica">
+                    <label for="politica">Política de tratamiento de datos</label>
+                </div>
+
+                <input type="submit" value="Registrarse">
+            </form>
+        </div>
+    </section>
+
+    <!-- footer -->
+    <footer class="footer">
+        <div class="footer__columns">
+            <div class="footer__column-big">
+                <h4 class="footer__column-title">Contáctanos</h4>
+
+                <ul class="footer__list">
+                    <li class="footer__item--dark">E-mail: comercial@qvision.us</li>
+                    <li class="footer__item--dark">Whatsapp: +300 255 02 65</li>
+                    <li class="footer__item">Comunícate con nosotros</li>
+                </ul>
+            </div>
+
+            <div class="footer__column">
+                <h4 class="footer__column-title">Categorías de Cursos</h4>
+
+                <ul class="footer__list">
+                    <li class="footer__item">Desarrollo</li>
+                    <li class="footer__item">Metodología de Pruebas</li>
+                    <li class="footer__item">Pruebas Funcionales</li>
+                    <li class="footer__item">Pruebas no Funcionales</li>
+                    <li class="footer__item">DevOps</li>
+                    <li class="footer__item">Cloud</li>
+                    <li class="footer__item">Automatización</li>
+                </ul>
+            </div>
+
+            <div class="footer__column">
+                <h4 class="footer__column-title">Certificaciones</h4>
+
+                <ul class="footer__list">
+                    <li class="footer__item">Scrum Master</li>
+                    <li class="footer__item">Scrum Product Owner</li>
+                    <li class="footer__item">Scrum Developer</li>
+                    <li class="footer__item">Agile Coach</li>
+                    <li class="footer__item">Design Thinking</li>
+                    <li class="footer__item">Kanban</li>
+                    <li class="footer__item">OKR</li>
+                    <li class="footer__item">DevOps</li>
+                </ul>
+            </div>
+
+            <div class="footer__column">
+            <h4 class="footer__column-title">Q-Vision Technologies</h4>
+
+                <ul class="footer__list">
+                    <li class="footer__item">Desarrollo de Software</li>
+                    <li class="footer__item">Aseguramiento de Calidad</li>
+                    <li class="footer__item">Automatización de Procesos con RPA</li>
+                    <li class="footer__item">Talento TI</li>
+                    <li class="footer__item">Gestión Inteligente de Datos</li>
+                </ul>
+            </div>
+
+            <div class="footer__column-big">
+                <h4 class="footer__column-title">Soporte</h4>
+
+                <ul class="footer__list">
+                    <li class="footer__item--dark">Si tienes inconvenientes o dudas, contáctanos al correo</li>
+                    <li class="footer__item--dark">izyacademy@qvision.us</li>
+                </ul>
+            </div>
+        </div>
+
+        <!-- parte inferior del footer -->
+        <div class="footer__bottom">
+            <div class="footer__bottom-top">
+                <div class="footer__bottom-logo">
+                    <img src="" alt="footer_logo">
+                </div>
+                <h5 class="footer__bottom">Términos y Condiciones - Política Tratamiento de Datos</h5>
+                <div class="footer__botom--social">
+                    <a href="" class="social__anchor"><i class="bi bi-facebook"></i></a>
+                    <a href="" class="social__anchor"><i class="bi bi-twitter"></i></a>
+                    <a href="" class="social__anchor"><i class="bi bi-instagram"></i></a>
+                    <a href="" class="social__anchor"><i class="bi bi-linkedin"></i></a>
+                </div>
+            </div>
+            <div class="footer__bottom-down">
+                <p>Copyright &copy; 2024 IzyAcademy Marca Registrada By Qvision Technologies.</p>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
